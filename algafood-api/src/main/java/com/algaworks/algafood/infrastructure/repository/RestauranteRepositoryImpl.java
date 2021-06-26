524a8dcfbfc1ab56447f389725066e4e640ee67f
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2021-06-26 09:01:03 -0300

    5.14. Adicionando restrições na cláusula where com Criteria API

--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -26,13 +29,19 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	@Override
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 		
-		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaBuilder builder 			= manager.getCriteriaBuilder();
+		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		Root<Restaurante> root 				= criteria.from(Restaurante.class);
 		
-		CriteriaQuery<Restaurante> criteriaQuery = builder.createQuery(Restaurante.class);
+		Predicate nomePredicate 		= builder.like(root.get("nome"), "%" + nome + "%"); 
+		Predicate taxaInicialPredicate	= builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFinalPredicate	= builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
 		
-		criteriaQuery.from(Restaurante.class);
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 				
-		return manager.createQuery(criteriaQuery).getResultList();
+		TypedQuery<Restaurante> query = manager.createQuery(criteria);
+		
+		return query.getResultList();
 	}
 
 	
