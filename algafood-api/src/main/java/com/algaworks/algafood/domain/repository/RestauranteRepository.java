aeab92f5f21afac95f2e66321989f50a6184e8e0
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2021-06-26 12:13:44 -0300

    5.17. Implementando Specifications com SDJ

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 //import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,7 +14,9 @@ import com.algaworks.algafood.domain.model.Restaurante;
 
 @Repository
 public interface RestauranteRepository 
-			extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries {
+			extends JpaRepository<Restaurante, Long>, 
+					RestauranteRepositoryQueries,
+					JpaSpecificationExecutor<Restaurante>{
 
 	//Podem também ser usadas outras palavras chave no início do método: find, read, query, stream, exists, count ou get.
 	
