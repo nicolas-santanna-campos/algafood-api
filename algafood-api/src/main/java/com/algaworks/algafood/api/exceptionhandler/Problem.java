0fcc7ae8450b0e9c7711755acfbbdb4895410f14
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2021-08-12 17:56:27 -0300

    9.4. Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -9,15 +10,22 @@ import lombok.Builder;
 import lombok.Getter;
 
 @JsonInclude(Include.NON_NULL)
-@Builder 
 @Getter
+@Builder
 public class Problem {
-	
+
+	private LocalDateTime timestamp;
 	private Integer status;
 	private String type;
 	private String title;
 	private String detail;
-	
 	private String userMessage;
-	private LocalDateTime timestamp;
+	private List<Field> fields;
+	
+	@Getter
+	@Builder
+	public static class Field {
+		private String name;
+		private String userMessage;
+	}
 }
