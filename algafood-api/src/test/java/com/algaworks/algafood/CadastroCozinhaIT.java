bd4d314f494c267171e11dc51cbc719aa785b562
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2021-08-21 21:36:49 -0300

    10.12. Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ algafood-api/src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -4,9 +4,11 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -21,12 +23,17 @@ public class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private Flyway flyway;
 
 	@Before
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
 	}
 	
 	@Test
@@ -53,7 +60,7 @@ public class CadastroCozinhaIT {
 	@Test
 	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
 		given()
-			.body("{\"nome\": \"Chinesa\" }")
+			.body("{\"titulo\": \"Chinesa\" }")
 			.contentType(ContentType.JSON)
 			.accept(ContentType.JSON)
 		.when()
