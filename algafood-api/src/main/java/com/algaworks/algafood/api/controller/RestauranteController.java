9149552fd37639a84aac566266f89bb34f9f5c9a
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2021-11-14 19:08:12 -0300

    11.17Mapeando para uma instância destino (não um tipo) com ModelMapper

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 //import org.springframework.validation.BeanPropertyBindingResult;
@@ -76,12 +75,13 @@ public class RestauranteController {
 								 @RequestBody @Valid RestauranteInput restauranteInput) {
 		try {
 			
-			Restaurante restaurante = restauranteInputDisassembler.toDomainObject(restauranteInput);
+			//Restaurante restaurante = restauranteInputDisassembler.toDomainObject(restauranteInput);
 			
 			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 			
-			BeanUtils.copyProperties(restaurante, restauranteAtual, 
-					"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
+			restauranteInputDisassembler.copyToDomainObject(restauranteInput, restauranteAtual);
+			
+			//BeanUtils.copyProperties(restaurante, restauranteAtual,	"id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 
 			return restauranteModelAssembler.toModel(cadastroRestaurante.salvar(restauranteAtual));
 		} catch (CozinhaNaoEncontradaException e) {
