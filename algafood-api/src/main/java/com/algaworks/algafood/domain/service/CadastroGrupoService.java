596600edf9f922f913b1ae7f490cc8b0e2d5c850
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2023-06-09 17:14:10 -0300

    12.15 Desafio implementando os endpoints de associação de grupos com permissões

--- algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroGrupoService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroGrupoService.java
@@ -10,6 +10,7 @@ import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.GrupoNaoEncontradoException;
 import com.algaworks.algafood.domain.model.Grupo;
 import com.algaworks.algafood.domain.repository.GrupoRepository;
+import com.algaworks.algafood.domain.model.Permissao;
 
 @Service
 public class CadastroGrupoService {
@@ -18,6 +19,9 @@ public class CadastroGrupoService {
 	
 	@Autowired
 	private GrupoRepository grupoRepository;
+	
+	@Autowired
+	private CadastroPermissaoService cadastroPermissao;
 	
 	@Transactional
 	public Grupo salvar(Grupo grupo) {
@@ -40,4 +44,20 @@ public class CadastroGrupoService {
 		return grupoRepository.findById(grupoId)
 				.orElseThrow(() -> new GrupoNaoEncontradoException(grupoId));
 	}
+	
+	@Transactional
+	public void desassociarPermissao(Long grupoId, Long permissaoId) {
+	    Grupo grupo = buscarOuFalhar(grupoId);
+	    Permissao permissao = cadastroPermissao.buscarOuFalhar(permissaoId);
+	    
+	    grupo.removerPermissao(permissao);
+	}
+
+	@Transactional
+	public void associarPermissao(Long grupoId, Long permissaoId) {
+	    Grupo grupo = buscarOuFalhar(grupoId);
+	    Permissao permissao = cadastroPermissao.buscarOuFalhar(permissaoId);
+	    
+	    grupo.adicionarPermissao(permissao);
+	} 
 }
